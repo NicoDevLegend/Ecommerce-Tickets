14d7e581df7b62350094fc4ad13a7b2a86392e36
Nicolás Enciso <121879206+nicoenciso@users.noreply.github.com>
2024-03-25 19:52:57 +0000

    v1

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongoose";
 import Product from "@/models/Product";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   await dbConnect();
   try {
